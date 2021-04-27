f941765f40498103646e29bf3a9cdd56a2e97d1c
Thomas Cowan <thomas.cowan@gmail.com>
2021-04-26 20:18:16 -0400

    React_POC: pure JS added to map config

---
+++ src/map-style.js
@@ -0,0 +1,13 @@
+export const dataLayer = {
+    id: 'data',
+    type: 'fill',
+    layout: {},
+    paint: {
+        'fill-color': {
+            type: 'identity',
+            property: 'color'
+        }, 
+        'fill-opacity': 0.6
+    },
+    maxzoom: 13.5
+}
