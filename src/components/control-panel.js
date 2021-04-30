a41d6277e041581575fd8bd8b1e9c59c51cd2218
Thomas Cowan <thomas.cowan@gmail.com>
2021-04-30 11:33:54 -0400

    VST-11: simple expand collapse control panel

--- src/components/control-panel.js
+++ src/components/control-panel.js
@@ -1,38 +1,53 @@
 import * as React from 'react';
 
+
 function ControlPanel(props) {
   const {year} = props;
 
-  return (
-    <div className="control-panel">
-      <h3>Vancouver Street Trees</h3>
-      <p>
-        Map showing street trees in Vancouver
-      </p>
-      <p>
-        Data source: <a href="https://bit.ly/3aj8Hdb">Vancouver Open Data</a>
-      </p>
-      <div className="source-link">
-        <a
-          href="https://github.com/tccw/yvr-street-trees"
-          target="_new"
-        >
-          View Code ↗
-        </a>
-      </div>
-      <hr />
+  const [isExpanded, setIsExpanded] = React.useState(true); 
 
-      <div key={'year'} className="input">
-        <label>Year</label>
-        <input
-          type="range"
-          value={year}
-          min={1995}
-          max={2015}
-          step={1}
-          onChange={evt => props.onChange(evt.target.value)}
-        />
-      </div>
+    const handleToggle = () => {
+        if (isExpanded) {
+            setIsExpanded(false);
+        } else {
+            setIsExpanded(true);
+        }
+    };
+
+  return (
+    <div className={isExpanded ? "control-panel" : "control-panel-collapsed"} onClick={handleToggle}>
+      {isExpanded && 
+        <>
+              <h3>Vancouver Street Trees</h3>
+          <p>
+            Map showing street trees in Vancouver
+          </p>
+          <p>
+            Data source: <a href="https://bit.ly/3aj8Hdb">Vancouver Open Data</a>
+          </p>
+          <div className="source-link">
+            <a
+              href="https://github.com/tccw/yvr-street-trees"
+              target="_new"
+            >
+              View Code ↗
+            </a>
+          </div>
+          <hr />
+
+          <div key={'year'} className="input">
+            <label>Year</label>
+            <input
+              type="range"
+              value={year}
+              min={1995}
+              max={2015}
+              step={1}
+              onChange={evt => props.onChange(evt.target.value)}
+            />
+          </div>
+        </>
+      }
     </div>
   );
 }
