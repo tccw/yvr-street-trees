e4800395a0165a5c47f62cfb03e38176520c4ffd
Thomas Cowan <thomas.cowan@gmail.com>
2021-05-22 01:56:07 -0400

    VST-18: clean up utils helpers

--- src/utils.js
+++ src/utils.js
@@ -30,7 +30,7 @@ export function heightStringFromID(height_range_id) {
 /**
  * Generates a properly formatted filter object to pass to a MapGL <Layer/> component
  * 
- * @param {number[]} diameters A list of diameters to filter by (should be the upper range of the 6 inche provided ranges)
+ * @param {number[]} diameters An array of the min and max diameter to filter to
  * @param {number[]} height_ids A list of height ids to filter by
  * @param {string[]} trees A list of tree common names to filter by
  * @returns {any[]} the formatted filter list ready to be passed to a MapGL <Layer/> component
@@ -39,7 +39,12 @@ export function heightStringFromID(height_range_id) {
   let filter = ['all'];
 
   if (diameters) {
-    filter.push(['all', ['>=', ['get', 'diameter'], diameters[0]], ['<=', ['get', 'diameter'], diameters[1]]]); // add the case expression to the filter
+    //TODO: change this so that the max diameter notch is not hardcoded
+    if (diameters[1] == 42) {
+      filter.push(['all', ['>=', ['get', 'diameter'], diameters[0]]]); // add the case expression to the filter
+    } else {
+      filter.push(['all', ['>=', ['get', 'diameter'], diameters[0]], ['<=', ['get', 'diameter'], diameters[1]]]); // add the case expression to the filter
+    }
   }
   
   if (height_ids) {
@@ -52,61 +57,6 @@ export function heightStringFromID(height_range_id) {
   
   return filter;
 }
-
-/**
- * Generates a properly formatted filter object to pass to a MapGL <Layer/> component
- * 
- * @param {number[]} diameters A list of diameters to filter by (should be the upper range of the 6 inche provided ranges)
- * @param {number[]} height_ids A list of height ids to filter by
- * @param {string[]} trees A list of tree common names to filter by
- * @returns {any[]} the formatted filter list ready to be passed to a MapGL <Layer/> component
- */
-export function _treeFilterCompositor({diameters, height_ids, trees}) {
-  let filter = ['all'];
-
-  if (diameters) {
-    filter.push(_buildMapboxCaseExpression(diameters)); // add the case expression to the filter
-  }
-  
-  if (height_ids) {
-    filter.push(['match', ['get','height_range_id'], height_ids, true, false]);
-  }
-
-  if (trees) {
-    filter.push(['match', ['get', 'common_name'], trees, true, false]);
-  }
-  
-  return filter;
-}
-
-
-/**
- * 
- * @param {number[]} diameters 
- * @returns {any[]} a case expression formatted for input to a MapBox Layer component
- */
-function _buildMapboxCaseExpression(diameters) {
-  let caseExpr = ['case'];
-    diameters.sort((a, b) => {return a - b}) // provice a comparator since sort() treats everyting as strings by default
-
-    // build the case expression in this format: https://docs.mapbox.com/mapbox-gl-js/style-spec/expressions/?size=n_10_n#case
-    diameters.forEach((diameter) => {
-      if (diameter === 6) {
-        caseExpr.push(['<', ['get', 'diameter'], 6]);
-        caseExpr.push(true);
-      } else if (diameter <= 42) {
-        caseExpr.push(['all', ['>=', ['get', 'diameter'], diameter - 6], ['<', ['get', 'diameter'], diameter]]);
-        caseExpr.push(true);
-      } else {
-        caseExpr.push(['>=', ['get', 'diameter'], 42]);
-        caseExpr.push(true);
-      }
-    });
-
-    caseExpr.push(false);  // push the default case output
-    return caseExpr;
-}
-
 
 /**
  * Takes a date in the format YYYY-MM-DD and returns
